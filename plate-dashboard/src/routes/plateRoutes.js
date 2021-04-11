97e25f2e8e0373adcd26f4f3ef62ee44ba669a8d
Michael Han <michaelhanwchs@gmail.com>
2021-04-11 15:22:52 -0600

    Added version
    -added v1 to api uris

--- plate-dashboard/src/routes/plateRoutes.js
+++ plate-dashboard/src/routes/plateRoutes.js
@@ -9,32 +9,32 @@ const routes = (app) => {
       //create post request
       .post(addNewOrder)
 
-    app.route('/v0/orders/:id')
+    app.route('/v1/orders/:id')
       // get specific order from id
       .get(getOrdersForOrderID)
-    app.route('/v0/orders/restaurant/:name')
+    app.route('/v1/orders/restaurant/:name')
       // get all orders by restaurant name
       .get(getOrdersForRestaurantName)
 
     //create route for inventory
-    app.route('/v0/inventory')
+    app.route('/v1/inventory')
       .post(addNewInventoryItem)
       
       .get(getInventoryItems)
 
-    app.route('/v0/inventory/:restaurantName')
+    app.route('/v1/inventory/:restaurantName')
       .get(getInventoryItemsForRestaurantName)
 
-    app.route('/v0/inventory/:restaurantName/:itemName')
+    app.route('/v1/inventory/:restaurantName/:itemName')
       .put(modifyInventoryItem)
 
-    app.route('/analytics/:restaurantName/menu_item_popularity')
+    app.route('/v0/analytics/:restaurantName/menu_item_popularity')
       .get(getCommonOrdersForRestaurantName)
 
     // Create route for creating menu items
-    app.route('/menu_items/')
+    app.route('/v1/menu_items/')
       .post(addNewMenuItem)
-    app.route('/menu_items/:restaurantName')
+    app.route('/v1/menu_items/:restaurantName')
       .get(getMenuItemsForRestaurantName)
 
   }
